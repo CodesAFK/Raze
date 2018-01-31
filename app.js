7340fa33764d1ec17250a0f97a3b77b91723b09e
CodesAFK <codesafk@gmail.com>
2018-01-31 08:21:43 -0600

    added express to app.js and server start function

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Works!");
+var express  = require('express');
+var app      = express();
+const server = 3000;
+
+
+
+
+
+//Start the server
+
+app.listen(server, function(){
+  console.log("Server has been started on " + server);
+});
